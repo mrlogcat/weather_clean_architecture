3cc0c63cf92a7abcf12ac8de8cc6cfcc4d4c27d6
unknown <mrlogcat@gmail.com>
2025-03-25 02:05:18 +0330

    add base usecase and GetCurrentWeatherUseCase

---
+++ lib/features/feature_weather/domain/use_cases/get_current_weather_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:weather_clean_architecture/core/resources/data_state.dart';
+import 'package:weather_clean_architecture/core/usecase/use_case.dart';
+import 'package:weather_clean_architecture/features/feature_weather/domain/entities/current_city_entity.dart';
+import 'package:weather_clean_architecture/features/feature_weather/domain/repository/weather_repository.dart';
+
+class GetCurrentWeatherUseCase extends UseCase<DataState<CurrentCityEntity>,String>{
+  final WeatherRepository repository;
+  GetCurrentWeatherUseCase(this.repository);
+
+  @override
+  Future<DataState<CurrentCityEntity>> call(String param) {
+    return repository.fetchCurrentWeatherData(param);
+  }
+
+
+}
